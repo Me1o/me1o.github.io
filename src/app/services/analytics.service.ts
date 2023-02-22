19e9b0ef7b7ea881cdcc6f56894558222da86b4b
me1o-GLT <106914826+me1oZid@users.noreply.github.com>
2023-02-22 12:44:50 +0200

    update

--- src/app/services/analytics.service.ts
+++ src/app/services/analytics.service.ts
@@ -18,21 +18,24 @@ export class AnalyticsService {
   ) { }
 
   boot() {
-    console.log('here')
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd),
       map(() => this.activeRoute),
       map(route => route.firstChild),
       switchMap(route => route!.data),
       map((data) => {
-
+        let postTitle = data.title;
+        const start = this.router.url.slice(0, 6);
+        if(start == '/post/'){
+          postTitle = this.router.url.replace(start, '');
+          postTitle = postTitle.replaceAll("-", " ");
+        }
         //here goes the GA reporting code
         this.gtag.pageview({
-          page_title: data && data.title ? data.title : this.default_title,
+          page_title: postTitle ? postTitle : this.default_title,
           page_path: this.router.url,
-          page_location: "www.me1o.io/" + this.router.url
+          page_location: "www.me1o.io" + this.router.url
         });
-
         return data && data.title ? `${data.title} • ${this.default_title}` : this.default_title;
       })
     ).subscribe((current_title) => this.title.setTitle(current_title));
