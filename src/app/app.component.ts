bc236f9f39fd13f43b94ee3a3f2cf67a1dee3d17
me1o-GLT <106914826+me1oZid@users.noreply.github.com>
2023-02-22 11:56:19 +0200

    analytics edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { AnalyticsService } from './services/analytics.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'artery-app';
+  constructor(private analyticsService: AnalyticsService) { }
+  ngOnInit() {
+    this.analyticsService.boot();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,8 @@ import { PostComponent } from './post/post.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-
+import {AnalyticsService} from "./services/analytics.service";
+import {GtagModule} from "angular-gtag";
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
     FooterComponent,
     NavbarComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, GtagModule.forRoot({ trackingId: 'G-G8CYCYXG39', trackPageviews: false })],
   providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent],
 })
